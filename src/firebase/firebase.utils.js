29e731f313d055778945b03c079b0a1cf56de24e
Brandon Tripp <brandongtripp@gmail.com>
2020-05-26 15:44:40 -0600

    implement code that pulls in our shop data from firestore and stores it in the shop reducer from our shop page. Also included spinnger HOC.

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -18,8 +18,6 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   const userRef = firestore.doc(`users/${userAuth.uid}`);
 
   const snapShot = await userRef.get();
-
-  console.log(snapShot.exists);
   
 
   if(!snapShot.exists){
@@ -32,14 +30,44 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
         email,
         createdAt,
         ...additionalData
-      })
+      });
     } catch (error) {
       console.log('error creating user', error.message);
     }
   }
 
   return userRef;
-};   
+};  
+
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+  const collectionRef = firestore.collection(collectionKey);
+
+  const batch = firestore.batch();
+  objectsToAdd.forEach(obj => {
+    const newDocRef = collectionRef.doc();
+    batch.set(newDocRef, obj);
+  });
+
+  return await batch.commit();
+};
+
+export const convertCollectionsSnapshotToMap = (collections) => {
+  const transformedCollection = collections.docs.map(doc => {
+    const { title, items } = doc.data();
+
+    return {
+      routeName: encodeURI(title.toLowerCase()),
+      id: doc.id,
+      title,
+      items
+    }
+  });
+  
+  return transformedCollection.reduce((accumulator, collection) => {
+    accumulator[collection.title.toLowerCase()] = collection;
+    return accumulator;
+  }, {})
+}
 
 firebase.initializeApp(config);
 
